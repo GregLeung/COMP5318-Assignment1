737b5f84208c05141a55ab684d7a6fe474e99baf
GregLeung <km0205hk@gmail.com>
2017-05-04 21:18:55 +1000

    Update MNBClassifier.py
    Added evaluation

--- MNBClassifier.py
+++ MNBClassifier.py
@@ -1,6 +1,14 @@
 import csv
 import time
 import math
+from random import randint
+
+
+# Read the selected features from preprocessing
+def readSelected(filename):
+    with open('Selected_features.txt') as f:
+        content = [int(x.strip('\n')) for x in f.readlines()]
+    return content
 
 
 # Read the files
@@ -10,13 +18,17 @@ def readCsvFile(filename):
 
 
 # Read training CSV files with app name and tf-idf values
-def readNameTfIdf(filename, alpha):
+def readNameTfIdf(filename, alpha, selected):
     rows = csv.reader(open(filename, "r"), delimiter=',')
     names = []
     data = list(rows)
     for i in range(len(data)):
+        removed = 0
         names.append(data[i][0])
         del data[i][0]
+        for ele in selected:
+            del data[i][ele - removed]
+            removed += 1
         data[i] = [float(x) + alpha for x in data[i]]
     return names, data
 
@@ -46,6 +58,13 @@ def groupByClass(training_data):
     return group
 
 
+# Length normalization
+def lengthNormalization(training_data):
+    divisor = sumAll(training_data)
+    for i in range(0, len(training_data)):
+        training_data[i] = [x / divisor for x in training_data[i]]
+
+
 # Calculate summation tf_idf for all words in a class
 def sumAll(classtfidf):
     result = 0.
@@ -69,7 +88,7 @@ def wordsProb(groups):
         divisor = 0.
         dividend = []
         for otherClass, otherValue in groups.items():
-            if otherClass != targetClass:
+            if targetClass != otherClass:
                 divisor += sumAll(otherValue)
                 dividend.append(sumWord(otherValue))
         dividend = sumWord(dividend)
@@ -96,13 +115,14 @@ def labelsProb(groups):
         training_data_size += len(key)
     result = {}
     for key, value in groups.items():
-        result[key] = math.log(float(len(key)) / float(training_data_size))
+        result[key] = float(len(key)) / float(training_data_size)
     return result
 
 
 # Calculate probability of each class and make prediction given the test dataset and training dataset. aka. step8
-def predict(test_name, test_data, wordsP):
+def predict(test_data, wordsP, labelsP):
     results = []
+    temp = {}
     for i in range(0, len(test_data)):
         minimum = float("inf")
         label = None
@@ -115,22 +135,59 @@ def predict(test_name, test_data, wordsP):
             if prob < minimum:
                 minimum = prob
                 label = key
-        print(result)
-        print(test_name[i], label)
+        if label not in temp:
+            temp[label] = 0
+        temp[label] += 1
         results.append(label)
+    for key, value in temp.items():
+        print(key, value)
+    return results
 
 
-def main():
-    # Training:
-    start = time.time()
-    training_names, training_data = readNameTfIdf('training_data - Copy.csv', 1)
-    labels = readCsvFile('training_labels.csv')
-    end = time.time()
-    print('Finish loading, the time used was: {0} seconds'.format(end - start))
-    start = time.time()
-    appendClass(training_names, training_data, labels)
-    end = time.time()
-    print('Finish appending class, the time used was: {0} seconds'.format(end - start))
+# Give each document a fold id
+def giveId(training_set, foldNum):
+    result = []
+    temp = [len(training_set) / foldNum] * foldNum
+    reminder = len(training_set) % foldNum
+    temp[0] += reminder
+    for i in range(0, len(training_set)):
+        fold = randint(0, 9)
+        while temp[fold] == 0:
+            fold = randint(0, 9)
+        temp[fold] -= 1
+        result.append(fold)
+    return result
+
+
+# Pick those doc which has certain fold id from the training set
+def pickFold(training_set, foldNum, foldID):
+    fold = []
+    foldLabel = []
+    training_copy = list(training_set)
+    removed = 0
+    for i in range(0, len(training_set)):
+        if foldID[i] == foldNum:
+            foldLabel.append(training_set[i][-1])
+            del training_copy[i - removed][-1]
+            fold.append(training_set[i])
+            del training_copy[i - removed]
+            removed += 1
+    return fold, foldLabel, training_copy
+
+
+# find out TP, TN, FP, FN
+def summary(predicted, reality):
+    TP = 0.
+    for i in range(len(predicted)):
+        print(predicted[i], reality[i])
+        if predicted[i] == reality[i]:
+            TP += 1
+    print(TP, len(predicted))
+    print('Accuracy:', TP / float(len(predicted)))
+
+
+# sub-routine for training
+def training(training_data):
     start = time.time()
     groups = groupByClass(training_data)
     end = time.time()
@@ -143,16 +200,51 @@ def main():
     weightNormalization(wordsP)
     end = time.time()
     print('Finish normalisation step, the time used was: {0} seconds'.format(end - start))
-
-    # Make prediction:
     start = time.time()
-    testing_names, testing_data = readNameTfIdf('test_data.csv', 0)
+    labelsP = labelsProb(groups)
+    end = time.time()
+    print('Finish calculating labelsProb step, the time used was: {0} seconds'.format(end - start))
+    return wordsP, labelsP
+
+
+# sub-routine for evaluation
+def evaulation(training_data):
+    start = time.time()
+    foldID = giveId(training_data, 10)
+    end = time.time()
+    print('Finish assigning fold id, the time used was: {0} seconds'.format(end - start))
+    start = time.time()
+    fold, foldLabel, rest = pickFold(training_data, 0, foldID)
+    end = time.time()
+    print('Finish picking fold, the time used was: {0} seconds'.format(end - start))
+    wordP, labelsP = training(rest)
+    predicted = predict(fold, wordP, labelsP)
+    summary(predicted, foldLabel)
+
+
+def main():
+    # Loading:
+    start = time.time()
+    features = readSelected('Selected_features.txt')
     end = time.time()
     print('Finish loading, the time used was: {0} seconds'.format(end - start))
     start = time.time()
-    predict(testing_names, testing_data, wordsP)
+    training_names, training_data = readNameTfIdf('training_data(5000).csv', math.pow(10, -1), features)
+    labels = readCsvFile('training_labels.csv')
     end = time.time()
-    print('Finish prediction, the time used was: {0} seconds'.format(end - start))
+    print('Finish loading, the time used was: {0} seconds'.format(end - start))
+    start = time.time()
+    lengthNormalization(training_data)
+    end = time.time()
+    print('Finish lengthNorm, the time used was: {0} seconds'.format(end - start))
+    # pre processing:
+    start = time.time()
+    appendClass(training_names, training_data, labels)
+    end = time.time()
+    print('Finish appending class, the time used was: {0} seconds'.format(end - start))
+    # Evaluation:
+    evaulation(training_data)
+    # Make prediction:
 
 
 main()
